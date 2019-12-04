776f9cedc46ae69f960fc76a503b38259a87caa7
Nacho Coloma <icoloma@gmail.com>
2019-12-04 23:18:41 +0100

    Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 // Jest config
 // See https://facebook.github.io/jest/docs/en/configuration.html
 module.exports = {
-  // load babel.config.js instead of .babelrc
-  //transform: { '^.+\\.js?$': '<rootDir>/src/main/es6/test/jest-transform.js' },
   testMatch: ["<rootDir>/test/MenuTest.js"],
   notify: true,
   verbose: true
